b0d65bd848e65abb20a70aefc0036a5ba2c06e75
Erick Araujo <erickaraujo4984@gmail.com>
2023-10-21 15:15:30 -0300

    Implementação da camada de dominio.
    Signed-off-by: Erick Araujo <erickaraujo4984@gmail.com>

---
+++ src/main/java/edu/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package edu.domain.model;
+
+import jakarta.persistence.Entity;
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+            
+}
---
+++ src/test/java/edu/SantanderBootcamp2023FullstackJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2023FullstackJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
