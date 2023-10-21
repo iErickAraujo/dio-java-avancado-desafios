85c4721c0d9fa453d2f5fc6dc0a637a8c76e317f
Erick Araujo <erickaraujo4984@gmail.com>
2023-10-21 15:23:15 -0300

    Hotfix 'precision' e 'scale.'
    Signed-off-by: Erick Araujo <erickaraujo4984@gmail.com>

--- src/main/java/edu/domain/model/Account.java
+++ src/main/java/edu/domain/model/Account.java
@@ -16,9 +16,10 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(scale = 13, precision = 2)
+    // 99.999.999.999,99
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "additional_limit",scale = 13, precision = 2)
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
